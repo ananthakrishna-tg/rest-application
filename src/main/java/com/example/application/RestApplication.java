34b8e155a5f77b49f61be665ec7d91a946d50a94
ananthakrishna-tg <ananthakrishna.udupa@gmail.com>
2023-01-07 16:12:40 +0530

    Renamed the class

--- src/main/java/com/example/application/RestApplication.java
+++ src/main/java/com/example/application/RestApplication.java
@@ -1,15 +1,16 @@
 package com.example.application;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"com.example.controller"})
-public class SpringApplication {
+public class RestApplication {
 
 	public static void main(String[] args) {
 
-		org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+		SpringApplication.run(RestApplication.class, args);
 		System.out.println("Running");
 	}
 
